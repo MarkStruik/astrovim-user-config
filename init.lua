e8601ddff249279068f25389a72f9b5eed18ca61
Mark Struik <markstruik@marks-imac.lan>
2022-11-14 22:28:26 +0100

    fixed the theme to load correctly

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end)
 return {
-  colorscheme = "catppuccin-", plugins = {
+  colorscheme = "catppuccin-macchiato", plugins = {
     init = {
       {
         "catppuccin/nvim",
