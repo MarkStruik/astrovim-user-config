8325938f18fb7a3c020ac163385352e28668ab83
Mark Struik <mark.struik@outlook.com>
2022-12-05 17:27:14 +0100

    added update vertical keybinds

--- init.lua
+++ init.lua
@@ -1,30 +1,45 @@
 vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end)
 return {
-  colorscheme = "catppuccin-macchiato", plugins = {
-    init = {
-      {
-        "catppuccin/nvim",
-        as = "catppuccin",
-        config = function()
-          require("catppuccin").setup {}
-        end,
-      },
+    colorscheme = "catppuccin-macchiato",
+    plugins = {
+        init = {
+            {
+                "catppuccin/nvim",
+                as = "catppuccin",
+                config = function()
+                    require("catppuccin").setup {}
+                end
+            }
+        }
     },
-  },
-  mappings = {
-    n = {
-      ["<leader>b"] = { "<cmd>%bd|e#|bd#<cr>", desc = "Close all buffer except current"},
+    lsp = {
+        on_attach = function(client, bufnr)
+            require('lspconfig').eslint.setup({
+                on_attach = function(client, bufnr)
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        buffer = bufnr,
+                        command = "EslintFixAll"
+                    })
+                end
+            })
+        end
     },
-  },
-  polish = function() 
-    local map = vim.api.nvim_set_keymap
-    local opts = { noremap = true, silent = true } 
-    map("n", "<C-J>", ":m .+1<cr>==", opts)
-    map("n", "<C-K>", ":m .-2<cr>==", opts)
-    vim.filetype.add {
-       extension = {
-         config = "xml",
-       }
-     }
-  end,
+    mappings = {
+        n = {
+            ["<leader>b"] = {
+                "<cmd>%bd|e#|bd#<cr>",
+                desc = "Close all buffer except current"
+            }
+        }
+    },
+    polish = function()
+        local map = vim.api.nvim_set_keymap
+        local opts = { noremap = true, silent = true }
+        -- map("n", "<C-J>", ":m .+1<cr>==", opts)
+        -- map("n", "<C-K>", ":m .-2<cr>==", opts)
+        map("n", "<C-d>", "<C-d>zz", opts)
+        map("n", "<C-u>", "<C-u>zz", opts)
+        vim.filetype.add { extension = { config = "xml" } }
+    end
 }
+-- nnoremap("<C-u>", "<C-u>zz")
